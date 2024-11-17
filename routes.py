9e556c1e76a6a5bc154763e3b9ded3e9714fbecb
Erkki Nurmi <183535537+eki-77@users.noreply.github.com>
2024-11-17 23:06:54 +0200

    Add calendar page

--- routes.py
+++ routes.py
@@ -8,5 +8,15 @@ def index():
     result = db.session.execute(text("SELECT id, calendarname FROM calendars"))
     calendars_id_name = result.fetchall()
     return render_template("index.html", calendars=calendars_id_name)
+
+@app.route("/calendar/<int:id>")
+def calendar(id):
+    sql = text("SELECT calendarname FROM calendars WHERE id=:id")
+    result = db.session.execute(sql, {"id":id})
+    a = result.fetchone()[0]
+    sql = text("SELECT * FROM events WHERE calendar_id=:id")
+    result = db.session.execute(sql, {"id":id})
+    events = result.fetchall()
+    return render_template("calendar.html", id=id, name=a, events=events)
     
 
