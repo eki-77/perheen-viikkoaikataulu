7728522887a4bf85c0c355dedf8b1f6e552b1631
Erkki Nurmi <183535537+eki-77@users.noreply.github.com>
2024-11-17 20:27:20 +0200

    Add index.html, routes module

--- app.py
+++ app.py
@@ -8,3 +8,6 @@ from werkzeug.security import check_password_hash, generate_password_hash
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 db = SQLAlchemy(app)
+
+import routes
+
