0542a0127dc407e3ee39ad3e6f2298d897c55fa3
Erkki Nurmi <183535537+eki-77@users.noreply.github.com>
2024-11-17 16:09:55 +0200

    Create flask app

--- app.py
+++ app.py
@@ -5,3 +5,6 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import text
 from werkzeug.security import check_password_hash, generate_password_hash
 
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+db = SQLAlchemy(app)
