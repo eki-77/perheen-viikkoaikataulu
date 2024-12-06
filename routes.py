f6fa51ca4c46b07e0e39a8f346e0c0b42a73844c
Erkki Nurmi <183535537+eki-77@users.noreply.github.com>
2024-12-06 14:20:50 +0200

    Reorganise imports and add access.py

--- routes.py
+++ routes.py
@@ -1,47 +1,11 @@
-from app import app
 from flask import redirect, render_template, request, session
-from db import db
 from sqlalchemy.sql import text
 from werkzeug.security import check_password_hash, generate_password_hash
 from secrets import token_hex
 
-def has_access(calendar_id):
-    if is_admin():
-        return True
-    if not session:
-        return False
-    username = session["username"]
-    sql = text("SELECT CO.user_id FROM calendar_owners CO, users U WHERE U.username=:username AND U.id = CO.user_id AND CO.calendar_id=:calendar_id")
-    result = db.session.execute(sql, {"username":username, "calendar_id":calendar_id})
-    rights = result.fetchone()
-    if rights:
-        return True
-    else:
-        return False
-    
-def is_admin():
-    if not session:
-        return False
-    #print(session)
-    #print(session["username"])
-    username = session["username"]
-    sql = text("SELECT admin FROM users WHERE username=:username")
-    result = db.session.execute(sql, {"username":username})
-    user = result.fetchone()
-    #print("admintesti, userin admintieto", user.admin)
-    return user.admin
-
-def create_admin_if_missing():
-    sql = text("SELECT 1 FROM users WHERE username=:admin")
-    result = db.session.execute(sql, {"admin":"admin"})
-    if not result.fetchone():
-        print("ei adminia!")
-        hash_value = generate_password_hash("tsoha-admin")
-        sql = text("INSERT INTO users (username, password, admin) VALUES (:username, :password, :admin)")
-        db.session.execute(sql, {"username":"admin", "password":hash_value, "admin":True})
-        db.session.commit()
-    else:
-        print("löytyi admin")
+from db import db
+from app import app
+from access import has_access, is_admin, create_admin_if_missing
 
 @app.route("/")
 def index():
